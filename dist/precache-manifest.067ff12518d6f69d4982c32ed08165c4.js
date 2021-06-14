3e5159aebc75763a283b00a12628da7180c56ff1
chatpeth.ke <chatpeth.ke@inet.co.th>
2021-06-14 16:25:26 +0700

    release demo v1

---
+++ dist/precache-manifest.067ff12518d6f69d4982c32ed08165c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b14d17056c61c76a2027",
+    "url": "/css/app.b7e7d812.css"
+  },
+  {
+    "revision": "760e0665529122dcf91e",
+    "url": "/css/chunk-vendors.091a3732.css"
+  },
+  {
+    "revision": "0b3c61f128a3ac3137fab349c2a8047e",
+    "url": "/img/Inet_logo.0b3c61f1.png"
+  },
+  {
+    "revision": "f4b4af9241467134bf7147100c5a93d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b14d17056c61c76a2027",
+    "url": "/js/app.d17e8284.js"
+  },
+  {
+    "revision": "760e0665529122dcf91e",
+    "url": "/js/chunk-vendors.5738422b.js"
+  },
+  {
+    "revision": "14e716b0d3d15c0bed1ff4f463b57fa8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
